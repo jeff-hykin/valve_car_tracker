58eac6f53d99ad2be08fc0addb166689b5b5e495
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-07 11:16:48 -0500

    indent

--- virkshop/events/@setup_with_system_tools/054_000_setup_python_venv.zsh
+++ virkshop/events/@setup_with_system_tools/054_000_setup_python_venv.zsh
@@ -5,9 +5,9 @@ export VIRTUAL_ENV="$PROJECT_FOLDER/.venv"
 export PATH="$VIRKSHOP_HOME/.local/bin:$PATH"
 if ! [ -d "$VIRTUAL_ENV" ]
 then
-    echo "creating virtual env for python"
+    echo "        creating virtual env for python"
     # run the cleanup
-    python -m venv "$VIRTUAL_ENV" && echo "virtual env created"
+    python -m venv "$VIRTUAL_ENV" && echo "        virtual env created"
 fi
 
 export PATH="$VIRTUAL_ENV/bin:$PATH"
