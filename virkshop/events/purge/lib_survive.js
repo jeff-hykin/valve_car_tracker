ed339a95fc688247981461c0bd5c851db71cbeee
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-10 08:05:12 -0500

    update quickr

--- virkshop/events/purge/lib_survive.js
+++ virkshop/events/purge/lib_survive.js
@@ -1,4 +1,4 @@
-import { FileSystem } from "https://deno.land/x/quickr@0.6.25/main/file_system.js"
+import { FileSystem } from "https://deno.land/x/quickr@0.6.26/main/file_system.js"
 
 const libSurviveConfigFolder = `${FileSystem.home}/.config/libsurvive/`
 console.log(`removing: ${libSurviveConfigFolder}`)
