44a39141a348a442d4434daf12f3fdb9859adbb7
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-03 10:46:42 -0500

    primitive controller working

--- repos/car_code/car_controller.py
+++ repos/car_code/car_controller.py
@@ -209,6 +209,7 @@ def test_wheels(wheels, cycles=1):
         time.sleep(1)
 
 class Car:
+    prev_drive_action = [0,0]
     def go(left_velocity, right_velocity):
         back_left_wheel.spin(left_velocity)
         front_left_wheel.spin(left_velocity)
@@ -219,39 +220,59 @@ class Car:
         """
             negative direction means left
         """
+        if Car.prev_drive_action == [velocity, direction]:
+            return
+        Car.prev_drive_action = [velocity, direction]
         velocity =  100 if velocity >  100 else velocity
         velocity = -100 if velocity < -100 else velocity
         direction     =  100 if direction     >  100 else direction
         direction     = -100 if direction     < -100 else direction
-        # print(f'''_''')
-        direction = (direction*abs(velocity/100))/2 # if velocity=0 then direction=0, then split the direction between each side
-        # print(f'''    direction = {direction}''')
-        left = velocity-direction  # 100-25
-        right = velocity+direction # 100+25
-        # print(f'''    inital left = {left}''')
-        # print(f'''    inital right = {right}''')
-        if right > 100:
-            extra = right-100 # 25
-            right = 100
-            left -= extra     # (100-25)-25
-        if right < -100:      
-            # left  = -100+25
-            # right = -100-25
-            extra = right+100 # -25
-            right = -100
-            left -= extra # (-100+25)-(-25) => -50
-        if left > 100:
-            extra = left-100 # 25
-            left = 100
-            right -= extra     # (100-25)-25
-        if left < -100:      
-            # right  = -100+25
-            # left = -100-25
-            extra = left+100 # -25
-            left = -100
-            right -= extra # (-100+25)-(-25) => -50
         
-        # print(f'''    left = {left}''')
-        # print(f'''    right = {right}''')
-        Car.go(left, right)
+        # direction = 100; slow_side = 0   * velocity
+        # direction = 50 ; slow_side = 0.5 * velocity
+        # direction = 0  ; slow_side = 1   * velocity
+        slow_side = (1-(abs(direction)/100)) * velocity
+        
+        print(f'''direction = {direction}''')
+        print(f'''velocity = {velocity}''')
+        print(f'''(abs(direction)/100) = {(abs(direction)/100)}''')
+        print(f'''(1-(abs(direction)/100)) = {(1-(abs(direction)/100))}''')
+        print(f'''slow_side = {slow_side}''')
+        
+        if direction < 0:
+            Car.go(slow_side, velocity)
+        else:
+            Car.go(velocity, slow_side)
+        
+        # # print(f'''_''')
+        # direction = (direction*abs(velocity/100))/2 # if velocity=0 then direction=0, then split the direction between each side
+        # # print(f'''    direction = {direction}''')
+        # left = velocity-direction  # 100-25
+        # right = velocity+direction # 100+25
+        # # print(f'''    inital left = {left}''')
+        # # print(f'''    inital right = {right}''')
+        # if right > 100:
+        #     extra = right-100 # 25
+        #     right = 100
+        #     left -= extra     # (100-25)-25
+        # if right < -100:      
+        #     # left  = -100+25
+        #     # right = -100-25
+        #     extra = right+100 # -25
+        #     right = -100
+        #     left -= extra # (-100+25)-(-25) => -50
+        # if left > 100:
+        #     extra = left-100 # 25
+        #     left = 100
+        #     right -= extra     # (100-25)-25
+        # if left < -100:      
+        #     # right  = -100+25
+        #     # left = -100-25
+        #     extra = left+100 # -25
+        #     left = -100
+        #     right -= extra # (-100+25)-(-25) => -50
+        
+        # # print(f'''    left = {left}''')
+        # # print(f'''    right = {right}''')
+        # Car.go(left, right)
         
