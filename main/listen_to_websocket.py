44a1a0f7e056ff06797092083536330934663dbf
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-03 08:24:39 -0500

    formatting

--- main/listen_to_websocket.py
+++ main/listen_to_websocket.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 
+
 async def listen_for_messages():
     uri = "ws://localhost:8080/ws"  # Change the URI to your WebSocket server
 
@@ -14,13 +15,21 @@ async def listen_for_messages():
                     print(
                         repr(
                             [
-                                f"{round(float(each), ndigits=1)}".ljust(6,"0")
-                                    for each in message.split(" ") if len(each) > 0 and (each[0].isdigit() or each[0]=="-") ][1:])+"     ", end="\r")
-                                    
-                                    # [       x_axis, y_axis, height_axis,      pitch, roll, yaw, roll    ]
-                    # print("Received message:", message, end="\r")
+                                f"{round(float(each), ndigits=1)}".ljust(6, "0")
+                                for each in message.split(" ")
+                                if len(each) > 0
+                                and (each[0].isdigit() or each[0] == "-")
+                            ][1:]
+                        )
+                        + "     ",
+                        end="\r",
+                    )
                     
+                    # [       x_axis, y_axis, height_axis,      pitch, roll, yaw, roll    ]
+                    # print("Received message:", message, end="\r")
+
         except websockets.exceptions.ConnectionClosedOK:
             print("WebSocket connection closed")
+
 
 asyncio.run(listen_for_messages())
