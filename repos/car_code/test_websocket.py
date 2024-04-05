aadf54e0a5083f05f41bde3e8bdbbf8c550601de
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-05 08:14:57 -0500

    send messages back

--- repos/car_code/test_websocket.py
+++ repos/car_code/test_websocket.py
@@ -5,17 +5,31 @@ from collections import defaultdict
 serve = websockets.serve
 
 # 
+# boilerplat for python await
+# 
+import threading
+values_to_await = []
+def awaiter():
+    while True:
+        if len(values_to_await) > 0:
+            # for some reason this is how to run await in a thread
+            values_to_await.pop().send(None)
+thread = threading.Thread(target=awaiter, args=())
+thread.start()
+
+# 
 # socket setup
 # 
 async def socket_response(websocket):
     async for message in websocket:
-        print(f'''websocket.path = {websocket.path}''')
-        print(f'''message = {message}''')
-            
+        values_to_await.append(
+            websocket.send("howdyyy")
+        )
 # 
 # start servers
 # 
 async def main():
+    print(f'''listening''')
     async with serve(socket_response, "localhost", 8080):
         await asyncio.Future()  # run forever
 
